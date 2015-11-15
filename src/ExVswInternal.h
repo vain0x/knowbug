72fd46fca42b27a73b6f940b517ff8e4ba44cb02
vain0 <id_vain@yahoo.co.jp>
2015-11-15 21:53:14 +0900

    Split declarations only for knowbug from `ExVardataString.h`

---
+++ src/ExVswInternal.h
@@ -0,0 +1,30 @@
+// VardataString writing functions (for uedai's Dlls)
+
+#pragma once
+
+#include <vector>
+#include "ExVardataString.h"
+
+#ifdef with_Assoc
+extern void WINAPI knowbugVsw_addValueAssoc(vswriter_t, char const* name, void const* ptr);
+#endif
+#ifdef with_Vector
+extern void WINAPI knowbugVsw_addVarVector(vswriter_t, char const* name, PVal const* pval);
+extern void WINAPI knowbugVsw_addValueVector(vswriter_t, char const* name, void const* ptr);
+#endif
+#ifdef with_Array
+extern void WINAPI knowbugVsw_addVarArray(vswriter_t, char const* name, PVal const* pval);
+extern void WINAPI knowbugVsw_addValueArray(vswriter_t, char const* name, void const* ptr);
+#endif
+#ifdef with_Modcmd
+extern void WINAPI knowbugVsw_addValueModcmd(vswriter_t, char const* name, void const* ptr);
+#endif
+
+struct VswInfoForInternal
+{
+	string vtname;
+	addVarUserdef_t addVar;
+	addValueUserdef_t addValue;
+};
+
+extern std::vector<VswInfoForInternal> const& vswInfoForInternal();
