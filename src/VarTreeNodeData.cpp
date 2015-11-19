36f89e987ca33a2dfed850bfd061b6a69cf60c9a
vain0 <id_vain@yahoo.co.jp>
2015-11-19 19:21:15 +0900

    Simplify ResultNodeData initializing

--- src/VarTreeNodeData.cpp
+++ src/VarTreeNodeData.cpp
@@ -8,6 +8,15 @@
 
 using WrapCall::ModcmdCallInfo;
 
+template<typename TWriter>
+static string stringFromResultData(ModcmdCallInfo const& callinfo, PDAT const* ptr, vartype_t vt)
+{
+	auto&& p = std::make_shared<CStrBuf>();
+	CVardataStrWriter::create<TWriter>(p)
+		.addResult(callinfo.stdat, ptr, vt);
+	return p->getMove();
+}
+
 ResultNodeData::ResultNodeData(ModcmdCallInfo::shared_ptr_type const& callinfo, PVal const* pvResult)
 	: ResultNodeData(callinfo, pvResult->pt, pvResult->flag)
 { }
@@ -16,19 +25,8 @@ ResultNodeData::ResultNodeData(ModcmdCallInfo::shared_ptr_type const& callinfo, 
 	: callinfo(callinfo)
 	, vtype(vt)
 	, pCallInfoDepended(callinfo->tryGetDependedCallInfo())
-{
-	{
-		auto p = std::make_shared<CStrBuf>();
-		CVardataStrWriter::create<CTreeformedWriter>(p)
-			.addResult(callinfo->stdat, ptr, vt);
-		treeformedString = p->getMove();
-	}
-	{
-		auto p = std::make_shared<CStrBuf>();
-		CVardataStrWriter::create<CLineformedWriter>(p)
-			.addResult(callinfo->stdat, ptr, vt);
-		lineformedString = p->getMove();
-	}
-}
+	, treeformedString(stringFromResultData<CTreeformedWriter>(*callinfo, ptr, vt))
+	, lineformedString(stringFromResultData<CLineformedWriter>(*callinfo, ptr, vt))
+{ }
 
 #endif //defined(with_WrapCall)
