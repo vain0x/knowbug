ef18d2b8a17b73d9c9edcd3d0a551f64db62e6f9
vain0 <id_vain@yahoo.co.jp>
2015-12-05 17:12:20 +0900

    Update make_package script

--- make_deploy_package.bat
+++ make_deploy_package.bat
@@ -1,9 +1,9 @@
 mkdir knowbug
-xcopy package /S /-Y knowbug
+xcopy package /S /-Y knowbug /EXCLUDE:package_exclude.txt
 copy changes.md knowbug\changes.md
 copy README.md  knowbug\README.md
 copy src\Release\hsp3debug.dll knowbug\hsp3debug_knowbug.dll
 copy src\x64\Release2\hsp3debug_64.dll knowbug\hsp3debug_64_knowbug.dll
+copy package\sample\VswFizzBuzz\Release\VswFizzBuzz.dll knowbug\sample\VswFizzBuzz\VswFizzBuzz.dll
 7za a knowbug-package.zip knowbug
-pause
 rmdir /S knowbug
