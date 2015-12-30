26c1495f51a29d381717e913c0d529201c313fe7
vain0 <id_vain@yahoo.co.jp>
2015-12-31 02:57:31 +0900

    Squashed 'src/tinytoml/' content from commit b0d1b1c
    git-subtree-dir: src/tinytoml
    git-subtree-split: b0d1b1cf735308cc229399a5dc60936e5f9551c1

---
+++ README.md
@@ -0,0 +1,85 @@
+# tinytoml
+
+A header only C++11 library for parsing TOML.
+
+[![Build Status](https://travis-ci.org/mayah/tinytoml.svg?branch=master)](https://travis-ci.org/mayah/tinytoml)
+
+This parser is based on TOML [v0.4.0](https://github.com/toml-lang/toml/blob/master/versions/en/toml-v0.4.0.md).
+This library is distributed under simplified BSD License.
+
+## Introduction
+
+tinytoml is a tiny [TOML](https://github.com/toml-lang/toml) parser for C++11 with following properties:
+- header file only
+- C++11 library friendly (array is std::vector, table is std::map, time is std::chrono::system_clock::time_point).
+- no external dependencies (note: we're using cmake for testing, but it's not required to use this library).
+
+We'd like to keep this library as handy as possible.
+
+## Prerequisite
+
+- C++11 compiler
+- C++11 libraries.
+
+I've only checked this library works with recent clang++ (3.5) and g++ (4.7). I didn't check this with cl.exe.
+Acutally I'm using this library on my Linux app and Mac app. However, I haven't written Windows app yet.
+
+## How to use
+
+Copy include/toml/toml.h into your project, and include it from your source. That's all.
+
+## Example code
+
+```c++
+std::ifstream ifs("foo.toml");
+toml::Parser parser(ifs);
+
+toml::Value v = parser.parse();
+// If toml file is valid, v would be valid.
+// Otherwise, you can get an error reason by calling Parser::errorReason().
+if (!v.valid()) {
+    cout << parser.errorReason() << endl;
+    return;
+}
+
+// You can find a value by find().
+// If found, non-null pointer will be returned.
+// You can check the type of value with is().
+// You can get the inner value by as().
+const toml::Value* x = v.find("bar");
+if (x && x->is<std::string>()) {
+    cout << x->as<string>() << endl;
+} else if (x && x->is<int>()) {
+    cout << x->as<int>() << endl;
+}
+
+// Note: the inner value of integer value is actually int64_t,
+// however, you can use 'int' for convenience.
+toml::Value* z = ...;
+int x = z->as<int>();
+int y = z->as<int64_t>();
+// toml::Array is actually std::vector<toml::Value>.
+// So, you can use range based for, etc.
+const toml::Array& ar = z->as<toml::Array>();
+for (const toml::Value& v : ar) {
+    ...
+}
+
+// For convenience way, you can use get() when you're sure that the value exists.
+// If type error occurred, std::runtime_error is raised.
+toml::Value v = ...;
+cout << v.get<string>("foo.bar") << endl;
+```
+
+## How to test
+
+The directory 'src' contains a few tests. We're using google testing framework, and cmake.
+
+```sh
+$ mkdir -p out/Debug; cd out/Debug
+$ cmake ../../src
+$ make
+$ make test
+```
+
+'src' also contains a small example for how to use this.
---
+++ src/value_test.cc
@@ -0,0 +1,276 @@
+#include "toml/toml.h"
+
+#include <gtest/gtest.h>
+
+#include <iostream>
+using namespace std;
+
+TEST(ValueTest, boolean)
+{
+    const toml::Value t(true);
+    const toml::Value f(false);
+
+    EXPECT_TRUE(t.is<bool>());
+    EXPECT_TRUE(f.is<bool>());
+
+    EXPECT_TRUE(t.as<bool>());
+    EXPECT_FALSE(f.as<bool>());
+
+    toml::Value v;
+    v = true;
+    EXPECT_TRUE(v.is<bool>());
+    EXPECT_TRUE(v.as<bool>());
+
+    v = false;
+    EXPECT_TRUE(v.is<bool>());
+    EXPECT_FALSE(v.as<bool>());
+
+    toml::Value x = t;
+    EXPECT_TRUE(x.is<bool>());
+    EXPECT_TRUE(x.as<bool>());
+}
+
+TEST(ValueTest, int)
+{
+    const toml::Value zero(0);
+    const toml::Value one(1);
+    const toml::Value mone(-1);
+
+    EXPECT_TRUE(zero.is<int>());
+    EXPECT_TRUE(one.is<int>());
+    EXPECT_TRUE(mone.is<int>());
+
+    EXPECT_EQ(0, zero.as<int>());
+    EXPECT_EQ(1, one.as<int>());
+    EXPECT_EQ(-1, mone.as<int>());
+
+    toml::Value v;
+    v = 100;
+    EXPECT_TRUE(v.is<int>());
+    EXPECT_EQ(100, v.as<int>());
+}
+
+TEST(ValueTest, int64_t)
+{
+    const toml::Value zero(static_cast<int64_t>(0));
+    const toml::Value one(static_cast<int64_t>(1));
+    const toml::Value mone(static_cast<int64_t>(-1));
+
+    EXPECT_TRUE(zero.is<int64_t>());
+    EXPECT_TRUE(one.is<int64_t>());
+    EXPECT_TRUE(mone.is<int64_t>());
+
+    // We accept is<int>() also.
+    EXPECT_TRUE(zero.is<int>());
+    EXPECT_TRUE(one.is<int>());
+    EXPECT_TRUE(mone.is<int>());
+
+    EXPECT_EQ(0, zero.as<int64_t>());
+    EXPECT_EQ(1, one.as<int64_t>());
+    EXPECT_EQ(-1, mone.as<int64_t>());
+
+    toml::Value v;
+    v = static_cast<int64_t>(100);
+    EXPECT_TRUE(v.is<int64_t>());
+    EXPECT_EQ(100, v.as<int64_t>());
+}
+
+TEST(ValueTest, double)
+{
+    const toml::Value zero(0.0);
+    const toml::Value one(1.0);
+    const toml::Value mone(-1.0);
+
+    EXPECT_TRUE(zero.is<double>());
+    EXPECT_TRUE(one.is<double>());
+    EXPECT_TRUE(mone.is<double>());
+
+    EXPECT_EQ(0.0, zero.as<double>());
+    EXPECT_EQ(1.0, one.as<double>());
+    EXPECT_EQ(-1.0, mone.as<double>());
+
+    toml::Value v;
+    v = 100.0;
+    EXPECT_TRUE(v.is<double>());
+    EXPECT_EQ(100.0, v.as<double>());
+}
+
+TEST(ValueTest, doubleWrite)
+{
+    {
+        const toml::Value v(1.0);
+        ostringstream ss;
+        v.write(&ss);
+        EXPECT_EQ("1.000000", ss.str());
+    }
+
+    {
+        const toml::Value v(10000000.0);
+        ostringstream ss;
+        v.write(&ss);;
+        EXPECT_EQ("10000000.000000", ss.str());
+    }
+
+    {
+        const toml::Value v(123456.789123);
+        ostringstream ss;
+        v.write(&ss);
+        EXPECT_EQ("123456.789123", ss.str());
+    }
+}
+
+TEST(ValueTest, string)
+{
+    toml::Value v1(string("foo"));
+    EXPECT_TRUE(v1.is<string>());
+    EXPECT_EQ("foo", v1.as<string>());
+
+    v1 = "test";
+    EXPECT_TRUE(v1.is<string>());
+    EXPECT_EQ("test", v1.as<string>());
+
+    v1 = string("kotori");
+    EXPECT_TRUE(v1.is<string>());
+    EXPECT_EQ("kotori", v1.as<string>());
+
+    toml::Value v2("foo");
+    EXPECT_TRUE(v2.is<string>());
+    EXPECT_EQ("foo", v2.as<string>());
+
+}
+
+TEST(ValueTest, time)
+{
+}
+
+TEST(ValueTest, array)
+{
+    toml::Value v;
+
+    // If we call push to null value, the value will become array automatically.
+    v.push(0);
+    v.push(1);
+
+    EXPECT_TRUE(v.is<toml::Array>());
+    EXPECT_EQ(0, v.get<int>(0));
+    EXPECT_EQ(1, v.get<int>(1));
+}
+
+TEST(ValueTest, arrayWrite)
+{
+    toml::Value v((toml::Array()));
+    {
+        ostringstream ss;
+        v.write(&ss);
+        EXPECT_EQ("[]", ss.str());
+    }
+
+    v.push(1);
+    {
+        ostringstream ss;
+        v.write(&ss);
+        EXPECT_EQ("[1]", ss.str());
+    }
+
+    v.push(2);
+    {
+        ostringstream ss;
+        v.write(&ss);
+        EXPECT_EQ("[1, 2]", ss.str());
+    }
+}
+
+TEST(ValueTest, table)
+{
+    toml::Value v;
+
+    // If we call set() to null value, the value will become table automatically.
+    v.set("key1", 1);
+    v.set("key2", 2);
+
+    EXPECT_EQ(1, v.get<int>("key1"));
+    EXPECT_EQ(2, v.get<int>("key2"));
+}
+
+TEST(ValueTest, table2)
+{
+    toml::Value v;
+
+    v.set("key1.key2", 1);
+    EXPECT_EQ(1, v.find("key1")->find("key2")->as<int>());
+}
+
+TEST(ValueTest, number)
+{
+    toml::Value v(1);
+    EXPECT_TRUE(v.isNumber());
+    EXPECT_EQ(1.0, v.asNumber());
+
+    v = 2.5;
+    EXPECT_TRUE(v.isNumber());
+    EXPECT_EQ(2.5, v.asNumber());
+
+    v = false;
+    EXPECT_FALSE(v.isNumber());
+}
+
+TEST(ValueTest, tableFind)
+{
+    toml::Value v;
+    v.set("foo", 1);
+
+    toml::Value* x = v.find("foo");
+    ASSERT_TRUE(x != nullptr);
+    *x = 2;
+
+    EXPECT_EQ(2, v.find("foo")->as<int>());
+}
+
+TEST(ValueTest, tableHas)
+{
+    toml::Value v;
+    v.set("foo", 1);
+    EXPECT_TRUE(v.has("foo"));
+    EXPECT_FALSE(v.has("bar"));
+}
+
+TEST(ValueTest, merge)
+{
+    toml::Value v1;
+    toml::Value v2;
+
+    v1.set("foo.foo", 1);
+    v1.set("foo.bar", 2);
+    v1.set("bar", 3);
+
+    v2.set("foo.bar", 4);
+    v2.set("foo.baz", 5);
+    v2.set("bar", 6);
+
+    EXPECT_TRUE(v1.merge(v2));
+
+    EXPECT_EQ(6, v1.get<int>("bar"));
+    EXPECT_EQ(1, v1.get<int>("foo.foo"));
+    EXPECT_EQ(4, v1.get<int>("foo.bar"));
+    EXPECT_EQ(5, v1.get<int>("foo.baz"));
+}
+
+TEST(ValueTest, arrayFind)
+{
+    toml::Value v;
+    v.push(1);
+
+    toml::Value* x = v.find(0);
+    ASSERT_TRUE(x != nullptr);
+    *x = 2;
+
+    EXPECT_EQ(2, v.find(0)->as<int>());
+}
+
+TEST(ValueTest, keyParsing)
+{
+    toml::Value v;
+    v.set("0000.0000", 1);
+    EXPECT_EQ(1, v.get<int>("0000.0000"));
+    EXPECT_EQ(1, v.find("0000")->get<int>("0000"));
+}
