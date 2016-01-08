f549cba39d89fce3481678fe844c90b3675a134c
vain0 <id_vain@yahoo.co.jp>
2016-01-09 00:49:59 +0900

    Remove unused func: `join` in strf.h

--- src/module/strf.h
+++ src/module/strf.h
@@ -14,16 +14,4 @@ static std::string strf(char const* format, Args&&... args) {
 	return fmt::sprintf(format, std::forward<Args>(args)...);
 }
 
-template<typename TIter>
-std::string join(TIter&& begin, TIter&& end, char const* delimiter)
-{
-	std::stringstream ss;
-	bool bFirst = true;
-	std::for_each(std::forward<TIter>(begin), std::forward<TIter>(end), [&](decltype(*begin) const& val) {
-		if ( bFirst ) { bFirst = false; } else { ss << delimiter; }
-		ss << val;
-	});
-	return ss.str();
-}
-
 #endif
