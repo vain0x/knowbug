ee4c7433177e4289db0685498d3b4c7d70f15e70
vain0x <vainzerox@gmail.com>
2019-06-16 09:59:46 +0900

    削除された機能のファイルを削除

--- make_deploy_package.bat
+++ make_deploy_package.bat
@@ -4,6 +4,5 @@ copy changes.md knowbug\changes.md
 copy README.md  knowbug\README.md
 copy src\Release\hsp3debug.dll knowbug\hsp3debug_knowbug.dll
 copy src\x64\Release2\hsp3debug_64.dll knowbug\hsp3debug_64_knowbug.dll
-copy package\sample\VswFizzBuzz\Release\VswFizzBuzz.dll knowbug\sample\VswFizzBuzz\VswFizzBuzz.dll
 7za a knowbug-package.zip knowbug
 rmdir /S knowbug
