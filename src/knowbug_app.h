f835d5bf649829e09299f71b1dfb78dca0d591fb
vain0x <vainzerox@gmail.com>
2019-07-16 21:53:22 +0900

    refactor: knowbug_app.h を整理

--- src/knowbug_app.h
+++ src/knowbug_app.h
@@ -1,38 +1,12 @@
-﻿//ごった煮:
-//windows APIまわりの宣言
-//hspsdkまわりの宣言
-//knowbugの宣言
+﻿//! Knowbug の本体
 
-#ifndef IG_HSP3DBGWIN_KNOWBUG_H
-#define IG_HSP3DBGWIN_KNOWBUG_H
+#pragma once
 
-#include <Windows.h>
-#undef min
-#undef max
-
-#include "hpiutil/hpiutil.hpp"
-#include "DebugInfo.h"
-#include "encoding.h"
-#include "module/utility.h"
 #include "StepController.h"
-
-using hpiutil::vartype_t;
-using hpiutil::varmode_t;
-using hpiutil::label_t;
-using hpiutil::csptr_t;
-using hpiutil::stdat_t;
-using hpiutil::stprm_t;
-using hpiutil::HSPVAR_FLAG_COMOBJ;
-using hpiutil::HSPVAR_FLAG_VARIANT;
-
-//extern HSPCTX* ctx; // declared and defined in hsp3plugin.(h/cpp)
-//extern HSPEXINFO* exinfo;
 
 class HspObjectPath;
 class HspRuntime;
-class KnowbugApp;
 class KnowbugView;
-class SourceFileResolver;
 
 class KnowbugApp {
 public:
@@ -57,5 +31,3 @@ public:
 
 	virtual void open_knowbug_repository() = 0;
 };
-
-#endif
