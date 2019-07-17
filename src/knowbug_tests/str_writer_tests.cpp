4857cc5ea3ae62ddc94975021092793ac10cd9bf
vain0x <vainzerox@gmail.com>
2019-07-17 22:20:45 +0900

    feat: メモリダンプ (16進数表示) をテスト

--- src/knowbug_tests/str_writer_tests.cpp
+++ src/knowbug_tests/str_writer_tests.cpp
@@ -89,4 +89,33 @@ void str_writer_tests(TestFramework& framework) {
 
 			return true;
 		});
+
+	suite.test(
+		u8"",
+		[&](TestCaseContext& t) {
+			auto w = str_writer_new();
+
+			auto t1 = as_utf8(u8"いろはにほへとちりぬるを");
+			auto t2 = as_utf8(u8"わかよたれそつねならむ");
+			auto size = t1.size() + 1 + t2.size() + 2;
+
+			auto buf = std::vector<Utf8Char>{};
+			buf.resize(size, Utf8Char{});
+			std::copy(t1.begin(), t1.end(), &buf[0]);
+			std::copy(t2.begin(), t2.end(), &buf[t1.size() + 1]);
+
+			w.catDump(buf.data(), buf.size());
+			w.catCrlf();
+
+			auto expected = as_utf8(
+				u8"dump  0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F\r\n"
+				u8"----------------------------------------------------\r\n"
+				u8"0000 E3 81 84 E3 82 8D E3 81 AF E3 81 AB E3 81 BB E3\r\n"
+				u8"0010 81 B8 E3 81 A8 E3 81 A1 E3 82 8A E3 81 AC E3 82\r\n"
+				u8"0020 8B E3 82 92 00 E3 82 8F E3 81 8B E3 82 88 E3 81\r\n"
+				u8"0030 9F E3 82 8C E3 81 9D E3 81 A4 E3 81 AD E3 81 AA\r\n"
+				u8"0040 E3 82 89 E3 82 80 00 00\r\n"
+			);
+			return t.eq(as_view(w), expected);
+		});
 };
