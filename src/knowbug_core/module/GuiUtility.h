5ae21212348c2b58e135ea3052010acf0a39cd13
vain0x <vainzerox@gmail.com>
2019-07-21 13:37:09 +0900

    refactor: 不要なコードを削除 (GuiUtility)

--- src/knowbug_core/module/GuiUtility.h
+++ src/knowbug_core/module/GuiUtility.h
@@ -20,13 +20,8 @@ void Window_SetTopMost(HWND hwnd, bool isTopMost);
 void Menu_ToggleCheck(HMENU menu, UINT itemId, bool& checked);
 
 void Edit_SetTabLength(HWND hEdit, const int tabwidth);
-void Edit_UpdateText(HWND hEdit, char const* s);
-void Edit_SetSelLast(HWND hEdit);
 
-auto TreeView_GetItemString(HWND hwndTree, HTREEITEM hItem) -> string;
-auto TreeView_GetItemLParam(HWND hwndTree, HTREEITEM hItem) -> LPARAM;
-void   TreeView_EscapeFocus(HWND hwndTree, HTREEITEM hItem);
-auto TreeView_GetChildLast(HWND hwndTree, HTREEITEM hItem) -> HTREEITEM;
+void TreeView_EscapeFocus(HWND hwndTree, HTREEITEM hItem);
 auto TreeView_GetItemAtPoint(HWND hwndTree, POINT pt) -> HTREEITEM;
 
 auto Dialog_SaveFileName(
