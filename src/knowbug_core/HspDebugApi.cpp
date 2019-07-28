f78fb3971e1ab8c441e2761024f7d3e0e60ea764
vain0x <vainzerox@gmail.com>
2019-07-28 23:02:45 +0900

    refactor: hsx::param_type_to_name を追加

--- src/knowbug_core/HspDebugApi.cpp
+++ src/knowbug_core/HspDebugApi.cpp
@@ -324,36 +324,7 @@ auto HspDebugApi::param_stack_to_data_at(HspParamStack const& param_stack, std::
 }
 
 auto HspDebugApi::param_type_to_name(HspParamType param_type) const -> char const* {
-	switch (param_type) {
-	case MPTYPE_LABEL:
-		return u8"label";
-
-	case MPTYPE_DNUM:
-		return u8"double";
-
-	case MPTYPE_LOCALSTRING:
-		return u8"str";
-
-	case MPTYPE_INUM:
-		return u8"int";
-
-	case MPTYPE_SINGLEVAR:
-		return u8"var";
-
-	case MPTYPE_ARRAYVAR:
-		return u8"array";
-
-	case MPTYPE_LOCALVAR:
-		return u8"local";
-
-	case MPTYPE_MODULEVAR:
-	case MPTYPE_IMODULEVAR:
-	case MPTYPE_TMODULEVAR:
-		return u8"modvar";
-
-	default:
-		return u8"???";
-	}
+	return hsx::param_type_to_name(param_type).value_or(u8"???");
 }
 
 auto HspDebugApi::param_data_to_type(HspParamData const& param_data) const -> HspParamType {
--- src/knowbug_core/hsx.h
+++ src/knowbug_core/hsx.h
@@ -70,6 +70,8 @@ namespace hsp_sdk_ext {
 
 	extern auto param_stack_to_param_data(HspParamStack const& param_stack, std::size_t param_index, HSPCTX const* ctx)->std::optional<HspParamData>;
 
+	extern auto param_type_to_name(HspParamType param_type)->std::optional<char const*>;
+
 	extern auto pval_to_type(PVal const* pval)->HspType;
 
 	extern auto pval_to_varmode(PVal const* pval)->HspVarMode;
