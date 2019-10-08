445c0ddbb2f26744813661acf3672bd5082de99a
vain0x <vainzerox@gmail.com>
2019-10-08 23:50:39 +0900

    refactor: 型エイリアスの名前を変更 (WindowHandlePtr など)

--- src/knowbug_dll/win_handles.h
+++ src/knowbug_dll/win_handles.h
@@ -3,35 +3,35 @@
 #include <memory>
 #include "../knowbug_core/platform.h"
 
-namespace Detail {
-
-struct module_deleter
-{
+struct WinFreeLibraryFn {
 	using pointer = HMODULE;
-	void operator()(HMODULE p) { FreeLibrary(p); }
+	void operator()(HMODULE p) {
+		FreeLibrary(p);
+	}
 };
 
-struct menu_deleter
-{
+struct WinDestroyMenuFn {
 	using pointer = HMENU;
-	void operator()(HMENU p) { DestroyMenu(p); }
+	void operator()(HMENU p) {
+		DestroyMenu(p);
+	}
 };
 
-struct window_deleter
-{
+struct WinDestroyWindowFn {
 	using pointer = HWND;
-	void operator()(HWND p) { DestroyWindow(p); }
+	void operator()(HWND p) {
+		DestroyWindow(p);
+	}
 };
 
-struct dgiobj_deleter
-{
+struct WinDeleteObjectFn {
 	using pointer = HGDIOBJ;
-	void operator()(pointer p) { DeleteObject(p); }
+	void operator()(pointer p) {
+		DeleteObject(p);
+	}
 };
 
-} //namespace Detail
-
-using module_handle_t = std::unique_ptr<HMODULE, Detail::module_deleter>;
-using menu_handle_t   = std::unique_ptr<HMENU,   Detail::menu_deleter>;
-using window_handle_t = std::unique_ptr<HWND,    Detail::window_deleter>;
-using gdi_obj_t       = std::unique_ptr<HGDIOBJ, Detail::dgiobj_deleter>;
+using ModuleHandlePtr = std::unique_ptr<HMODULE, WinFreeLibraryFn>;
+using MenuHandlePtr = std::unique_ptr<HMENU, WinDestroyMenuFn>;
+using WindowHandlePtr = std::unique_ptr<HWND, WinDestroyWindowFn>;
+using GdiObjHandlePtr = std::unique_ptr<HGDIOBJ, WinDeleteObjectFn>;
