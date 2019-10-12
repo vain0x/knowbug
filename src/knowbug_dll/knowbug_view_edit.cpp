c2f321e6d47b8544c13c517af1880e3f9b30c0e1
vain0x <vainzerox@gmail.com>
2019-10-13 00:14:41 +0900

    refactor: ファイルを分割 (knowbug_view_edit)

---
+++ src/knowbug_dll/knowbug_view_edit.cpp
@@ -0,0 +1,48 @@
+#include "pch.h"
+#include "knowbug_view_edit.h"
+
+class ViewEditControlImpl
+	: public AbstractViewBox
+{
+	HWND view_edit_;
+
+public:
+	ViewEditControlImpl(HWND view_edit)
+		: view_edit_(view_edit)
+	{
+	}
+
+	auto current_scroll_line() const -> std::size_t override {
+		return Edit_GetFirstVisibleLine(view_edit_);
+	}
+
+	bool at_bottom() const override {
+		auto line_count = Edit_GetLineCount(view_edit_);
+
+		// ウィンドウに30行ぐらい表示されていると仮定して、スクロールが一番下にありそうかどうか判定する。
+		return line_count <= (int)current_scroll_line() + 30;
+	}
+
+	void scroll_to_line(std::size_t line_index) override {
+		Edit_Scroll(view_edit_, (int)line_index, 0);
+	}
+
+	void scroll_to_bottom() override {
+		auto line_count = Edit_GetLineCount(view_edit_);
+		scroll_to_line(line_count);
+	}
+
+	void select_line(std::size_t line_index) override {
+		auto start = Edit_LineIndex(view_edit_, (int)line_index);
+		auto end = Edit_LineIndex(view_edit_, (int)line_index + 1);
+		Edit_SetSel(view_edit_, start, end);
+	}
+
+	void set_text(OsStringView const& text) override {
+		SetWindowText(view_edit_, text.data());
+	}
+};
+
+auto AbstractViewBox::create(HWND view_edit)->std::unique_ptr<AbstractViewBox> {
+	return std::make_unique<ViewEditControlImpl>(view_edit);
+}
---
+++ src/knowbug_dll/knowbug_view_edit.h
@@ -0,0 +1,25 @@
+//! オブジェクトの詳細を表示するエディットコントロール
+
+#pragma once
+
+#include "../knowbug_core/encoding.h"
+
+class AbstractViewBox {
+public:
+	static auto create(HWND view_edit)->std::unique_ptr<AbstractViewBox>;
+
+	virtual ~AbstractViewBox() {
+	}
+
+	virtual auto current_scroll_line() const->std::size_t = 0;
+
+	virtual bool at_bottom() const = 0;
+
+	virtual void scroll_to_line(std::size_t line_index) = 0;
+
+	virtual void scroll_to_bottom() = 0;
+
+	virtual void select_line(std::size_t line_index) = 0;
+
+	virtual void set_text(OsStringView const& text) = 0;
+};
