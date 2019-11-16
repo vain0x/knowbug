105bc602c8b64464f89b7a7dc3d02500a61bfc5f
vain0x <vainzerox@gmail.com>
2019-11-16 17:48:58 +0900

    chore: クライアントのビルドスクリプトを改善
    - ビルド時にウィンドウが一瞬表示される現象を解消
    	- ビルド用のスクリプトを一度実行ファイルにすることで packopt hide が効くようにした。
    - スクリプトへのパラメーターを一時ファイルではなくコマンドラインで渡すように変更

--- scripts/build-client.ps1
+++ scripts/build-client.ps1
@@ -1,4 +1,5 @@
 # knowbug_client をビルドする。
+
 # 注意: pwsh で GUI アプリを起動するとき、終了を待機しないことがある。
 #      アプリをパイプの途中で実行するときは待機されるので、
 #      xxx.exe (GUIアプリ) が終了するのを待ちたいときは xxx.exe | out-null と書く。
@@ -13,48 +14,34 @@ if (!$env:KNOWBUG_CLIENT_HSP3_ROOT) {
     exit 1
 }
 
+$workDir = (get-item .).fullName
+$serverHspRoot = (get-item $env:KNOWBUG_SERVER_HSP3_ROOT).fullName
+$clientHspRoot = (get-item $env:KNOWBUG_CLIENT_HSP3_ROOT).fullName
+
 ./scripts/hsp3-make.ps1
 if (!$?) {
     write-error 'hsp3-make.ps1 が失敗しました。'
     exit 1
 }
 
-$workDir = (get-item .).fullName
-try {
-    $makeAx = "$workDir/scripts/hsp3_make.ax"
-    $makeFile = "$workDir/src/knowbug_client/hsp3_makefile"
-    if (!(test-path $makeAx)) {
-        write-error "$makeAx が見つかりません。"
-        exit 1
-    }
-
-    # クライアントをビルドする。
-    cd "$workDir/src/knowbug_client"
-    echo "$workDir/src/knowbug_client/kc_main.hsp" >$makeFile
-    echo $env:KNOWBUG_SERVER_HSP3_ROOT >>$makeFile
-    & "$env:KNOWBUG_SERVER_HSP3_ROOT/hsp3.exe" $makeAx | out-null
-    if (!$?) {
-        write-error 'knowbug_client のビルドに失敗しました。'
-        exit 1
-    }
-
-    # プロキシをビルドする。
-    cd "$workDir/src/knowbug_client"
-    echo "$workDir/src/knowbug_client/kc_main_proxy.hsp" >$makeFile
-    echo $env:KNOWBUG_SERVER_HSP3_ROOT >>$makeFile
-    & "$env:KNOWBUG_SERVER_HSP3_ROOT/hsp3.exe" $makeAx | out-null
-    if (!$?) {
-        write-error 'knowbug_client_proxy のビルドに失敗しました。'
-        exit 1
-    }
-
-    # プロキシの設定ファイルを作成する。
-    $proxyConfigPath = "$env:KNOWBUG_SERVER_HSP3_ROOT/knowbug_client_proxy.txt"
-    echo "$workDir/src/knowbug_client/kc_main.hsp" >$proxyConfigPath
-    echo $env:KNOWBUG_CLIENT_HSP3_ROOT >>$proxyConfigPath
-
-    # プロキシを配置する。
-    copy-item -force "$workDir/src/knowbug_client/knowbug_client_proxy.exe" "$env:KNOWBUG_SERVER_HSP3_ROOT/knowbug_client.exe"
-} finally {
-    cd $workDir
+# クライアントをビルドする。
+& "$clientHspRoot/hsp3_make.exe" "$workDir/src/knowbug_client/kc_main.hsp" "$workDir/src/knowbug_client" $clientHspRoot | out-null
+if (!$?) {
+    write-error 'knowbug_client のビルドに失敗しました。'
+    exit 1
 }
+
+# プロキシをビルドする。
+& "$clientHspRoot/hsp3_make.exe" "$workDir/src/knowbug_client/kc_main_proxy.hsp" "$workDir/src/knowbug_client" $clientHspRoot | out-null
+if (!$?) {
+    write-error 'knowbug_client_proxy のビルドに失敗しました。'
+    exit 1
+}
+
+# プロキシの設定ファイルを作成する。
+$proxyConfigPath = "$serverHspRoot/knowbug_client_proxy.txt"
+echo "$workDir/src/knowbug_client/kc_main.hsp" >$proxyConfigPath
+echo $clientHspRoot >>$proxyConfigPath
+
+# プロキシを配置する。
+copy-item -force "$workDir/src/knowbug_client/knowbug_client_proxy.exe" "$serverHspRoot/knowbug_client.exe"
--- scripts/hsp3-make.ps1
+++ scripts/hsp3-make.ps1
@@ -7,17 +7,25 @@ if (!$env:KNOWBUG_CLIENT_HSP3_ROOT) {
 }
 
 $workDir = (get-item .).fullName
-try {
-    cd './scripts'
+$clientHspRoot = (get-item $env:KNOWBUG_CLIENT_HSP3_ROOT).fullName
 
-    $makeScript = (get-item 'hsp3_make.hsp').fullName
+# 実行ファイル生成用のスクリプトをコンパイルする。
+& "$clientHspRoot/hspcmp.exe" "--compath=$clientHspRoot/common/" "$workDir/scripts/hsp3_make_cli.hsp"
+if (!$?) {
+    write-error 'hsp3_make_cli.hsp のコンパイルに失敗しました。'
+    exit 1
+}
 
-    # 実行ファイル生成用のスクリプトをコンパイルする。
-    & "$env:KNOWBUG_CLIENT_HSP3_ROOT/hspcmp.exe" "--compath=$env:KNOWBUG_CLIENT_HSP3_ROOT\\common\\" $makeScript
-    if (!$?) {
-        write-error 'hsp3_make.hsp のコンパイルに失敗しました。'
-        exit 1
-    }
-} finally {
-    cd $workDir
+# AXファイルが生成されたことを確認する。
+$makeAx = "$workDir/scripts/hsp3_make_cli.ax"
+if (!(test-path $makeAx)) {
+    write-error "$makeAx が見つかりません。"
+    exit 1
+}
+
+# 実行ファイルを生成するための実行ファイルを生成する。
+& "$clientHspRoot/hsp3cl.exe" $makeAx "$workDir/scripts/hsp3_make.hsp" $clientHspRoot $clientHspRoot
+if (!$?) {
+    write-error 'hsp3_make.hsp の実行ファイル生成に失敗しました。'
+    exit 1
 }
