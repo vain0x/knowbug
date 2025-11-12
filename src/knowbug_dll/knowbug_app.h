cd79b96bdebabf478d7fa3a74fdf1d45cb494d5f
vain0x <vainzerox@gmail.com>
2025-11-13 00:00:39 +0900

    refactor: コードを移動 (WindowHandle 型など)

--- src/knowbug_dll/knowbug_app.h
+++ src/knowbug_dll/knowbug_app.h
@@ -16,3 +16,31 @@ public:
 
 	virtual auto objects()->HspObjects & = 0;
 };
+
+// -----------------------------------------------
+// ヘルパー
+// -----------------------------------------------
+
+class Win32CloseHandleFn {
+public:
+	using pointer = HANDLE;
+
+	void operator()(HANDLE p) {
+		CloseHandle(p);
+	}
+};
+
+class Win32DestroyWindowFn {
+public:
+	using pointer = HWND;
+
+	void operator()(HWND p) {
+		DestroyWindow(p);
+	}
+};
+
+using ProcessHandle = std::unique_ptr<HANDLE, Win32CloseHandleFn>;
+
+using ThreadHandle = std::unique_ptr<HANDLE, Win32CloseHandleFn>;
+
+using WindowHandle = std::unique_ptr<HWND, Win32DestroyWindowFn>;
--- src/knowbug_dll/knowbug_server.cpp
+++ src/knowbug_dll/knowbug_server.cpp
@@ -45,34 +45,6 @@ static auto knowbug_version() -> std::u8string {
 }
 
 // -----------------------------------------------
-// ヘルパー
-// -----------------------------------------------
-
-class Win32CloseHandleFn {
-public:
-	using pointer = HANDLE;
-
-	void operator()(HANDLE p) {
-		CloseHandle(p);
-	}
-};
-
-class Win32DestroyWindowFn {
-public:
-	using pointer = HWND;
-
-	void operator()(HWND p) {
-		DestroyWindow(p);
-	}
-};
-
-using ProcessHandle = std::unique_ptr<HANDLE, Win32CloseHandleFn>;
-
-using ThreadHandle = std::unique_ptr<HANDLE, Win32CloseHandleFn>;
-
-using WindowHandle = std::unique_ptr<HWND, Win32DestroyWindowFn>;
-
-// -----------------------------------------------
 // 隠しウィンドウ
 // -----------------------------------------------
 
